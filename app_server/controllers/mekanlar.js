9256b8e1fbe8e5d9e679bd8de0ee05563878ad49
busraknya <busrakonya5@gmail.com>
2022-10-13 16:23:48 +0300

    odev2

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+const anaSayfa = function (req, res, next) {
+  res.render('anasayfa', { title: 'Ana sayfa' });
+}
+
+const mekanBilgisi = function (req, res) {
+  res.render('mekanbilgisi', { "title": "Mekan bilgisi" });
+}
+
+const yorumEkle = function (req, res) {
+  res.render('yorumekle', { "title": "Yorum ekle" });
+}
+
+
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle
+}
